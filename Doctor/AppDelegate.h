1c463aefbccc6dd0ad693a1b5a94a58733004a99
wangyitian <18514616528@163.com>
2017-02-09 18:33:57 +0800

    Initial Commit

---
+++ Doctor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Doctor
+//
+//  Created by 王翼天 on 2017/2/9.
+//  Copyright © 2017年 王翼天. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Doctor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Doctor
+//
+//  Created by 王翼天 on 2017/2/9.
+//  Copyright © 2017年 王翼天. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
