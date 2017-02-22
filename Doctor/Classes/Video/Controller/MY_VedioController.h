c4fdb25bcead296cd6fe15a1451240589f841fc0
wangyitian <18514616528@163.com>
2017-02-22 18:02:48 +0800

    加直播模块。

---
+++ Doctor/Classes/Video/Controller/MY_VedioController.h
@@ -0,0 +1,13 @@
+//
+//  MY_VedioController.h
+//  Doctor
+//
+//  Created by 王翼天 on 2017/2/22.
+//  Copyright © 2017年 王翼天. All rights reserved.
+//
+
+#import "MY_BaseController.h"
+
+@interface MY_VedioController : MY_BaseController
+
+@end
---
+++ Doctor/Classes/Video/Controller/MY_VedioController.m
@@ -0,0 +1,37 @@
+//
+//  MY_VedioController.m
+//  Doctor
+//
+//  Created by 王翼天 on 2017/2/22.
+//  Copyright © 2017年 王翼天. All rights reserved.
+//
+
+#import "MY_VedioController.h"
+
+@interface MY_VedioController ()
+
+@end
+
+@implementation MY_VedioController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
